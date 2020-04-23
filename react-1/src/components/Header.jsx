f3da4ddabf71b0ffbe95552d7aba7421ffa405c9
scertyy <sw1ple@mail.ru>
2020-04-23 17:50:49 +0300

    Kek

---
+++ react-1/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/1000px-Apple_logo_black.svg.png" />
+        </header>
+    )
+}
+
+export default Header;
---
+++ react-1/src/components/Profile.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg" />
+            </div>
+            <div>
+                ava + descrpition
+            </div>
+            <div>
+                My posts
+                <div>
+                    New posts
+                </div>
+                <div>
+                    post1
+                </div>
+                <div>
+                    post2
+                </div>
+                <div>
+                    post3
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+
+export default Profile;
